1ae2e243d761655d6418e24a8cf52b9ca683c32d
mkarki1 <77561953+mkarki1@users.noreply.github.com>
2021-06-03 17:54:19 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o6dm4*m%r73j@oxp13=32(zb-x#^*w!h0@-mi7l#+_sl=po1+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
